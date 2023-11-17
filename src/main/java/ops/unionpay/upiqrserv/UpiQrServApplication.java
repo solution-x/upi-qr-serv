4fced10a283a6a0a9b28535429e19775ac1b6780
solution-x <rangaprasad7@gmail.com>
2023-11-17 14:12:21 +0800

    first change

--- src/main/java/ops/unionpay/upiqrserv/UpiQrServApplication.java
+++ src/main/java/ops/unionpay/upiqrserv/UpiQrServApplication.java
@@ -11,3 +11,18 @@ public class UpiQrServApplication {
 	}
 
 }
+
+
+package ops.unionpay.upiqrserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpiQrServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpiQrServApplication.class, args);
+	}
+
+}
